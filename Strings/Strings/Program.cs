fb389ad0b8930c6a1257a752fc79b16b4f66fb3e
NajmAlzorqah <najmalzorqah@gmail.com>
2024-08-20 23:37:43 +0300

    Strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Najm Aldeen  ";
+            Console.WriteLine("Trim: '{0}'" , fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'" , fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var fName = fullName.Substring(0, index);
+            var lName = fullName.Substring(index + 1);
+            Console.WriteLine("First Name: " + fName);
+            Console.WriteLine("last Name: " + lName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " + names[1]);
+
+            
+            Console.WriteLine(fullName.Replace("Aldeen", "Alzorqah"));
+
+            if (String.IsNullOrEmpty(null))
+            {
+                Console.WriteLine("1 - Invalid");
+            }
+
+            if (String.IsNullOrEmpty(" "))
+            {
+                Console.WriteLine("2 - Invalid");
+            }
+            if (String.IsNullOrEmpty(" ".Trim()))
+            {
+                Console.WriteLine("3 - Invalid");
+            }
+
+             
+
+
+        }
+    }
+}
---
+++ Working With Dates/Working With Dates/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Working_With_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Date and time are immutable 
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now; 
+            var today = DateTime.Today;
+            
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+            var tomrrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+            Console.WriteLine("Tomrrow: " + tomrrow);
+            Console.WriteLine("Yesterday: " + yesterday);
+            
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd"));
+            
+            // ======== Time Spn =========
+
+            //Creating 
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start; // this returns timespan 
+            Console.WriteLine( "Duration: " + duration);
+
+            // Properties 
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            // Add 
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)).TotalMinutes);
+
+
+            // Subtract
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)).TotalMinutes);
+
+            //ToString
+            Console.WriteLine("ToString: " + timeSpan.ToString());
+
+            // Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+
+        }
+    }
+}
